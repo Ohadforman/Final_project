75d0b6191a0b682d66c356d5baabd48f785368fd
Ohad_Forman <ohadforman@mail.tau.ac.il>
2022-11-15 15:25:01 +0200

    del

--- ASSISTANS_FUNCTIONS/open_local_mlx.m
+++
@@ -1,6 +0,0 @@
-function open_local_mlx(mlxname, lineNum)
-    whence = mfilename('fullpath');
-    [filedir, basename] = fileparts(whence);
-    mlxname = fullfile(filedir, [mlxname '.mlx']);
-    matlab.desktop.editor.openAndGoToLine(mlxname, lineNum);
-end
--- README.md
+++
@@ -1,3 +0,0 @@
-# Final_project
-
-This is Ohad's Forman Final project in EEE degree in Tel Aviv University.
